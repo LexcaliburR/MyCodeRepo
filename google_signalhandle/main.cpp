e2ac4c3119248297b19255b581c0737ce8dc3f4e
lexcaliburr <289427380@gmail.com>
2024-04-19 13:50:16 +0800

    feat(google signalhandle): add google signalhandler

---
+++ google_signalhandle/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * @Author: lexcaliburr 289427380@gmail.com
+ * @Date: 2023-09-27 16:15:09
+ * @LastEditors: lexcaliburr 289427380@gmail.com
+ * @LastEditTime: 2023-09-27 16:15:39
+ * @FilePath: /google_signalhandle/main.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+#include "glog/logging.h"
+
+#include "package1.h"
+#include "package2.h"
+
+int main()
+{
+    LOG(INFO) << "111111111";
+}
